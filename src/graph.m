82e2a345f8787358a738c9bcc679fa64eb24d2b6
ylu95 <ylu95@gatech.edu>
2014-04-23 17:32:06 -0400

    matlab graph

--- src/graph.m
+++ src/graph.m
@@ -2,32 +2,58 @@
 % system('make');
 % system('./project');
 % 
-t = tcpip('0.0.0.0',3000,'NetworkRole','server');
+t = tcpip('0.0.0.0',3000,'NetworkRole','server','Terminator','');
 set(t,'InputBufferSize',120000);
+%set(t,'ByteOrder','littleEndian')
 t
 disp('Waiting for connection')
-fopen(t);
-disp('fopen returned!')
-data = fread(t,1,'char')
-fclose(t);
-t
-return;
-disp('Going into read loop')
-while true
-    channelNum = fread(t,1,'uint32')
-    t
-    size = fread(t,1,'uint32')
-    t
-    arraySize = size;
-    data = fread(t,arraySize,'uint32');
-    t
-end
-data = fscanf(t)
-fclose(t);
-delete(t);
-clear t;
+%fopen(t);
 
-exit;
+disp('Going into read loop')
+
+data = zeros(8,5000);
+while true
+    for i = 1:8
+        %channelNum = fread(t,1,'uint32');
+        %size = fread(t,1,'uint32');
+        size=1000;
+        arraySize = size;
+        %data(i) = fread(t,arraySize,'uint32');
+    end
+    
+    scrsz = get(0,'ScreenSize');
+    hfig = figure;
+    set(hfig,'position',scrsz);
+    
+    subplot(8,1,1)
+    plot(1:arraySize-1, data(1,1:arraySize-1));
+    title('Channel 1');
+    subplot(8,1,2)
+    plot(1:arraySize-1, data(1,1:arraySize-1));
+    title('Channel 2');
+    subplot(8,1,3)
+    plot(1:arraySize-1, data(1,1:arraySize-1));
+    title('Channel 3');
+    subplot(8,1,4)
+    plot(1:arraySize-1, data(1,1:arraySize-1));
+    title('Channel 4');
+    subplot(8,1,5)
+    plot(1:arraySize-1, data(1,1:arraySize-1));
+    title('Channel 5');
+    subplot(8,1,6)
+    plot(1:arraySize-1, data(1,1:arraySize-1));
+    title('Channel 6');
+    subplot(8,1,7)
+    plot(1:arraySize-1, data(1,1:arraySize-1));
+    title('Channel 7');
+    subplot(8,1,8)
+    plot(1:arraySize-1, data(1,1:arraySize-1));
+    title('Channel 8');
+    xlabel('Sample Number');
+end
+
+fclose(t);
+return;
 
 c1 = dlmread('channel1.csv', ',');
 c2 = dlmread('channel2.csv', ',');
@@ -39,10 +65,8 @@ c7 = dlmread('channel7.csv', ',');
 c8 = dlmread('channel8.csv', ',');
 
 scrsz = get(0,'ScreenSize');
-
 hfig = figure;
 set(hfig,'position',scrsz);
-
 
 subplot(8,1,1)
 plot(c1(:,1), c1(:,2));
